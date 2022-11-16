295232e2f719b893498b78dd84613273ccabb0d1
Gauthier Beaumont <gauthierbeaumont@gmail.com>
2022-11-16 16:30:56 +0100

    passage des méthodes dans le view model

--- BoardGameTools/Client/ViewModels/ICardViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BoardGameTools.Client.Models;
-
-namespace BoardGameTools.Client.ViewModels
-{
-    public interface ICardViewModel
-    {
-        public Task<List<CardModel>> GetCard();
-    }
-}
